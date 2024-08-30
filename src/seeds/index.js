b7af248802b77a4c4bad423cd5681c79bd59fac4
Gabriel F de  Carvalho <143818850+GabrielGFC@users.noreply.github.com>
2024-08-30 19:32:02 -0300

    :boom: fix: Revertendo mudanças ineficientes

--- src/seeds/index.js
+++ src/seeds/index.js
@@ -1,28 +1,8 @@
 import usuarioSeed from './usuario.js';
-import alertaSeed from './alerta.js';
-import graduacaoSeed from './graduacao.js';
-import unidadeSeed from './unidade.js';
-import qrcodeSeed from './qrcode.js';
-import efetivoSeed from './efetivo.js'
-import postoSeed from './posto.js';
-import moduloSeed from './modulo.js';
-import veiculoSeed from './veiculo.js';
+
 
 const seed = async () => {
-	//Efetivo
-	await qrcodeSeed();
-	await alertaSeed();
-	await graduacaoSeed();
-	await unidadeSeed();
-	await efetivoSeed();
-
 	//Usuario
-	await moduloSeed();
-	await usuarioSeed();
-
-	//Registro
-	await postoSeed();
-	await veiculoSeed();
+		await usuarioSeed();
 };
-
 export default seed;
